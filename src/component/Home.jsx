d65c4ef1793043ed38c16f549247285f06fbc754
Rene1couvin <renefrancisco808@gmal.com>
2024-03-27 14:21:10 +0200

    pure

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -49,45 +49,43 @@ const Home = () => {
 
   return (
     <div className='px-8'>
-      
+
       <div className='px-4 bg-beast'>
-        <div className='flex bg-beast'>
-          <div className='w-1/2  py-28'>
+        <div className='flex flex-col md:flex-row bg-beast'>
+          <div className='md:w-1/2 py-8 md:py-28'>
             <p className='text-2xl'>FIND MOVIES</p>
             <p className='text-4xl font-sans font-bold beast'>TV SHOW AND MORE</p>
 
             <p className='py-4'>
               Movies are a powerful medium that captivate audiences through storytelling, visuals, and sound. They transport viewers to different worlds, evoke a range of emotions, and provoke thought and reflection. From gripping dramas that explore the depths of human experience to heartwarming comedies that offer moments of laughter and joy, movies cater to diverse tastes and interests.
             </p>
-            <button className='flex rounded-xl border-2 border-white px-4 py-2'><img src={sv} alt="sv" className=" h-[40px]" /><p className='pt-2 px-2'>Watch Tutorial</p></button>
-            <div className=' items-center  py-8'>
-              <div className='flex items-center  py-8'>
-                <input
-                  type='text'
-                  placeholder='Search movies...'
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  className='px-4 py-2 border border-gray-400 rounded-md mr-2'
-                />
-                <button
-                  onClick={handleSearch}
-                  className='px-4 py-2 bg-blue-500 text-white rounded-md'
-                >
-                  Search
-                </button>
-              </div>
+            <button className='flex rounded-xl border-2 border-white px-4 py-2'><img src={sv} alt="sv" className="h-[40px]" /><p className='pt-2 px-2'>Watch Tutorial</p></button>
+            <div className='flex items-center py-4 md:py-8'>
+              <input
+                type='text'
+                placeholder='Search movies...'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className='px-4 py-2 border border-gray-400 rounded-md mr-2'
+              />
+              <button
+                onClick={handleSearch}
+                className='px-4 py-2 bg-blue-500 text-white rounded-md'
+              >
+                Search
+              </button>
             </div>
           </div>
-          <div className='w-1/2 relative ml-32 '>
-
-            <img src={im2} alt='im2' className="absolute  w-[20rem] h-[450px] top-0 mt-18 ml-[10rem] " />
+          <div className='md:w-1/2 relative ml-0 md:ml-32'>
+            <img src={im2} alt='im2' className="absolute w-[20rem] h-[450px] top-0 mt-18 ml-[10rem]" />
             <div className='relative w-[20rem] h-[450px] mt-20 ml-[2rem]'>
-              <img src={im1} alt='im1' className="absolute  w-full h-full object-cover  " />
-              <img src={sv} alt="sv" className="absolute top-0 bottom-0 right-0 left-0 mx-auto my-auto  w-32 h-32 " />
+              <img src={im1} alt='im1' className="absolute w-full h-full object-cover" />
+              <img src={sv} alt="sv" className="absolute top-0 bottom-0 right-0 left-0 mx-auto my-auto w-32 h-32" />
             </div>
-
           </div>
         </div>
+
+
         <div className='w-full '>
           {searchResults.length > 0 && (
             <div className="flex justify-center place-content-center items-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
@@ -115,13 +113,13 @@ const Home = () => {
         {movies.map((movie, index) => (
           <NavLink to={`/detail/${movie.id}`}>
 
-          <Card
-            key={index}
-            title={movie.original_title}
-            price={movie.vote_average}
-            vote={movie.vote_count}
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          />
+            <Card
+              key={index}
+              title={movie.original_title}
+              price={movie.vote_average}
+              vote={movie.vote_count}
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            />
           </NavLink>
         ))}
       </div>
